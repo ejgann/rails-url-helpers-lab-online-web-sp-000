fd4cf3eff2898d59ff0e5d0c538107df7275c31f
Emily G <aspentree79@gmail.com>
2020-01-24 01:31:07 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
@@ -10,14 +10,9 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
-    @student.active = !@student.active
-    @student.save 
-    redirect_to student_path(@student) 
+    @student.active= !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
 end
